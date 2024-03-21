beb148c5fe881cdd50ab510718f6fe87f9eaaa06
xinyuji <quennel1115@gmail.com>
2024-03-21 15:04:07 +0800

    chore: add config for deploy github pages

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -8,18 +8,19 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://quennelcoder.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/quennelBlog',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'quennelOrg', // Usually your GitHub org/user name.
+  projectName: 'quennelBlog', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
