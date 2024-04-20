b56be87dd2c37aaeb962d0f15821b97442d563c7
xinyuji <quennel1115@gmail.com>
2024-04-20 10:46:43 +0800

    feat: add initial weather style

--- my-website/src/components/weatherFeature/main.tsx
+++ my-website/src/components/weatherFeature/main.tsx
@@ -8,11 +8,11 @@ import ForecastWeather from '@site/src/components/weatherFeature/viewController/
 import { PlaceType } from '@site/src/components/address/model';
 
 const Item = styled(Paper)(({ theme }) => ({
-	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	// backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
 	...theme.typography.body2,
 	padding: theme.spacing(1),
-	textAlign: 'center',
-	color: theme.palette.text.secondary,
+	// textAlign: 'center',
+	// color: theme.palette.text.secondary,
 }));
 const WeatherFeature = () => {
 	const [currentWeatherData, setCurrentWeatherData] = useState<PlaceType | null>(null);
@@ -29,12 +29,12 @@ const WeatherFeature = () => {
 						<CitySearch />
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={12}>
 					<Item>
 						<CurrentWeather />
 					</Item>
 				</Grid>
-				<Grid item xs={6}>
+				<Grid item xs={12}>
 					<Item>
 						<ForecastWeather />
 					</Item>
